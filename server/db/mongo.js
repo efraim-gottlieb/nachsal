ccaa15b2411d7e72f26474266368dc4aecb89c5f
EfraimGottlieb <efgo583208979@gmail.com>
2026-03-25 22:29:04 +0200

    s

--- server/db/mongo.js
+++ server/db/mongo.js
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 
 export async function connectDB() {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      maxPoolSize: 10,
+      serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 45000,
+    });
     console.log("MongoDB connected");
   } catch (err) {
     console.error("MongoDB connection error:", err.message);
